fb2813884893442633230def34acf5b366b78d00
arparker5 <arparker5@alaska.edu>
2019-01-25 20:56:12 -0900

    Added countTo function

--- SC_gittest/main.cpp
+++ SC_gittest/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <string>
+using std::cout;
+using std::endl;
+using std::cin;
 
-using namespace std;
+void countTo(int x)
+{
+    for(int i = 1; i <= x; i++)
+        cout<<i<<endl;
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int x;
+    cout<<"Input a number: "<<endl;
+    cin>> x;
+    if(cin)
+        countTo(x);
     return 0;
 }
